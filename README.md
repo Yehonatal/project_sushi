316bcec4da54f836a2d2b08127f42c46121b87e3
yonatan afewerk <yonatanafewerk@gmail.com>
2024-03-11 11:20:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# project_sushi + Restaurant App
---
+++ android/app/src/main/kotlin/com/example/project_sushi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_sushi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
